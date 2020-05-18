7c32531b71f632a7640a6453b6664664f55d23a9
andika-118140058 <64176009+andika-118140058@users.noreply.github.com>
2020-05-18 23:57:49 +0700

    Add files via upload

---
+++ tiketpesawat/Tiket.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiketpesawat;
+
+
+public class Tiket {
+    public String kelas_pesawat;
+    public int jumlah_tiket;
+    public int harga_tiket;
+    public int total_biaya;
+    public int uang_bayar;
+    public int uang_kembali;
+
+}
